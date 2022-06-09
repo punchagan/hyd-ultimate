6db26e7feb84d04c54910e2f29c504d764786f66
Puneeth Chaganti <punchagan@muse-amuse.in>
2022-06-09 18:54:23 +0530

    Add initial form and JS

---
+++ js/places.js
@@ -0,0 +1,28 @@
+const setupSearchBox = () => {
+  const inputBox = document.querySelector("#inputAddress");
+  const defaultBounds = new google.maps.LatLngBounds(
+    new google.maps.LatLng(17.1867065, 78.5185524),
+    new google.maps.LatLng(17.1867065, 78.5185524)
+  );
+  const searchBox = new google.maps.places.SearchBox(inputBox, {
+    // Doesn't restrict the results, but changes the ordering...
+    bounds: defaultBounds
+  });
+
+  const updateLocation = () => {
+    const location = searchBox.getPlaces()[0];
+    if (location) {
+      const coords = {
+        lat: location.geometry.location.lat(),
+        lng: location.geometry.location.lng()
+      };
+      console.log(coords);
+      const locationBox = document.querySelector("#inputLocation");
+      const text = `${coords.lat},${coords.lng}`;
+      locationBox.value = text;
+    }
+  };
+  google.maps.event.addListener(searchBox, "places_changed", updateLocation);
+};
+
+window.setupSearchBox = setupSearchBox;
